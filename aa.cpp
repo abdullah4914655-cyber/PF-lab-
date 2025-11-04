653a561ea0015d81b8da81de0a0d5a4817e91787
ABDULLAH AMJAD HUSSAIN <abdullah4914655@icloud.com>
2025-11-04 08:59:18 +0500

    version 1.1

---
+++ aa.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2,sum,diff,prod,quot;
+	cout<<"enter num 1";
+	cin>>num1;
+	cout<<"enter num 2";
+	cin>>num2;
+	
+	sum=num1+num2;
+	diff=num1-num2;
+	prod=num1*num2;
+	quot=num1/num2;
+	
+	cout<<"sum is :"<<sum<<endl;
+	cout<<"diff is :"<<diff<<endl;
+	cout<<"prod is :"<<prod<<endl;
+	cout<<"quot is :"<<quot<<endl;
+	
+	return 0;
+	
+}
